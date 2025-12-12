b9be7946c28c2d768308d2413b74c9d674d96a78
JavaharGaneshMutthu <142410740+JavaharGaneshMutthu@users.noreply.github.com>
2025-12-12 17:02:02 +0530

    Modify server.xml port settings in entrypoint script
    Updated port replacement comment and disabled shutdown port.

--- render-entrypoint.sh
+++ render-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-# Replace port in server.xml with Render’s $PORT
+# Replace HTTP port 8080 with Render's $PORT
 sed -i "s/port=\"8080\"/port=\"${PORT}\"/g" $CATALINA_HOME/conf/server.xml
+
+# Disable shutdown port (set to -1)
+sed -i 's/Server port="8005"/Server port="-1"/g' $CATALINA_HOME/conf/server.xml
 
 exec catalina.sh run
